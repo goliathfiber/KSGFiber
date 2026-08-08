dd59b9764f710d49655496abf591774b4011c241
goliathfiber <patrick.kovack@gmail.com>
2026-08-08 12:02:42 -0500

    Configure Bluehost /kramer test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ContactModalProvider>
         <Routes>
           <Route element={<Layout />}>
